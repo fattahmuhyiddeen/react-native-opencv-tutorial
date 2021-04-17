f2edd894dfbe277928d6c433110ec96207381ba3
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2021-04-17 17:19:16 +0800

    Update downloadAndInsertOpenCV.sh

--- downloadAndInsertOpenCV.sh
+++ downloadAndInsertOpenCV.sh
@@ -1,3 +1,5 @@
+# https://jaist.dl.sourceforge.net/project/opencvlibrary/4.5.2/opencv-4.5.2-ios-framework.zip
+
 # taken from https://github.com/brainhubeu/react-native-opencv-tutorial/blob/master/downloadAndInsertOpenCV.sh
 
 # ios
@@ -5,22 +7,23 @@ version=4.5.2
 # base_url=https://razaoinfo.dl.sourceforge.net/project/opencvlibrary/${version}/
 base_url=https://jaist.dl.sourceforge.net/project/opencvlibrary/${version}
 
-curl ${base_url}/opencv-${version}-ios-framework.zip -o opencv-${version}-ios-framework.zip
-unzip -a opencv-${version}-ios-framework.zip
+filename=opencv-${version}-ios-framework.zip
+curl ${base_url}/${filename} -o ${filename}
+unzip -a ${filename}
 cd ios
 cp -r ./../opencv2.framework ./
 cd ..
-rm -rf opencv-${version}-ios-framework.zip
+rm -rf ${filename}
 rm -rf opencv2.framework/
 
 # android
+filename=opencv-${version}-android-sdk.zip
 
-curl ${base_url}/opencv-${version}-android-sdk.zip -o opencv-${version}-android-sdk.zip
-unzip opencv-${version}-android-sdk.zip
+curl ${base_url}/${filename} -o ${filename}
+unzip ${filename}
 cd android/app/src/main
 mkdir jniLibs
 cp -r ./../../../../OpenCV-android-sdk/sdk/native/libs/ ./jniLibs
 cd ../../../../
-rm -rf opencv-${version}-android-sdk.zip
+rm -rf ${filename}
 rm -rf OpenCV-android-sdk/
-
